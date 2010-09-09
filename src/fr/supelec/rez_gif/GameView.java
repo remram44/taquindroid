100ecacbf159affb9d076fce690fbbfabf9ab056
remram44 <remirampin@gmail.com>
2010-09-09 02:51:09 +0200

    Touch detection done.
    --HG--
    extra : transplant_source : .%8F%87%03%9BA-k%08%15%05%FF%80%C8%EA%F2%02%29%AFM

--- src/fr/supelec/rez_gif/GameView.java
+++ src/fr/supelec/rez_gif/GameView.java
@@ -3,6 +3,7 @@ package fr.supelec.rez_gif;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -20,7 +21,9 @@ public class GameView extends View {
 
     private int m_Width, m_Height;
     private Block[] m_Blocks = null;
-    private final Paint m_Paint;
+    private int m_ActiveBlock = -1;
+    private int[][] m_Grid = null;
+    private final Paint m_Paint = new Paint();
     private int m_TileSize;
 
     public GameView(Context context, int width, int height)
@@ -28,26 +31,30 @@ public class GameView extends View {
         super(context);
         m_Width = width;
         m_Height = height;
-        m_Paint = new Paint();
 
         // Create the blocks
         m_Blocks = new Block[m_Width * m_Height - 1];
+        m_Grid = new int[m_Width][m_Height];
         for(int y = 0; y < m_Height; ++y)
             for(int x = 0; x < m_Width; ++x)
             {
                 if(y == m_Height-1 && x == m_Width - 1)
-                    continue;
-                m_Blocks[y*m_Width+x] = new Block();
-                m_Blocks[y*m_Width+x].num = y*3 + x + 1;
-                m_Blocks[y*m_Width+x].x = x;
-                m_Blocks[y*m_Width+x].y = y;
+                    m_Grid[x][y] = -1;
+                else
+                {
+                    m_Grid[x][y] = y*m_Width + x;
+                    m_Blocks[y*m_Width+x] = new Block();
+                    m_Blocks[y*m_Width+x].num = y*3 + x + 1;
+                    m_Blocks[y*m_Width+x].x = x;
+                    m_Blocks[y*m_Width+x].y = y;
+                }
             }
     }
 
     protected void onSizeChanged(int w, int h, int oldw, int oldh)
     {
-        int tileSizeX = (int) Math.floor(w/m_Width);
-        int tileSizeY = (int) Math.floor(h/m_Height);
+        int tileSizeX = (int)Math.floor(w/m_Width);
+        int tileSizeY = (int)Math.floor(h/m_Height);
         m_TileSize = Math.min(tileSizeX, tileSizeY);
     }
 
@@ -58,7 +65,10 @@ public class GameView extends View {
         {
             for(int i = 0; i < m_Blocks.length; ++i)
             {
-                m_Paint.setColor(0xFFFFFFFF);
+                if(m_ActiveBlock == i)
+                    m_Paint.setColor(0xFFFF0000);
+                else
+                    m_Paint.setColor(0xFFFFFFFF);
                 int x1 = (int)Math.round(m_Blocks[i].x  ) * m_TileSize + 2;
                 int x2 = (int)Math.round(m_Blocks[i].x+1) * m_TileSize - 2;
                 int y1 = (int)Math.round(m_Blocks[i].y  ) * m_TileSize + 2;
@@ -76,6 +86,30 @@ public class GameView extends View {
                         m_Paint);
             }
         }
+    }
+
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if(event.getAction() == MotionEvent.ACTION_UP)
+        {
+            m_ActiveBlock = -1;
+            invalidate();
+        }
+        else if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            // Find the correct block
+            int x = (int)Math.round(event.getX());
+            int y = (int)Math.round(event.getY());
+            if(x < m_Width * m_TileSize && y < m_Height * m_TileSize)
+            {
+                int u = (int)Math.floor(x/m_TileSize);
+                int v = (int)Math.floor(y/m_TileSize);
+                m_ActiveBlock = m_Grid[u][v];
+                GameView.this.invalidate();
+                return true;
+            }
+        }
+        return false;
     }
 
 }
