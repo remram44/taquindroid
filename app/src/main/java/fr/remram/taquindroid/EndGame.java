4112caecbcbcd79006b0e8f64567da808d1c433a
Remi Rampin <remirampin@gmail.com>
2016-04-20 20:40:57 -0400

    Make proper endgame view (with image)

--- app/src/main/java/fr/remram/taquindroid/EndGame.java
+++ app/src/main/java/fr/remram/taquindroid/EndGame.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class EndGame extends Activity {
@@ -13,22 +13,24 @@ public class EndGame extends Activity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.endgame);
 
         // Get the parameters from the Indent
         Intent intent = getIntent();
         int width = intent.getIntExtra("width", 3);
         int height = intent.getIntExtra("height", 3);
-        
+
         // Find the requested image
         Uri selectedImage = intent.getData();
-        
-        // TODO : Display game time
-        // TODO : Draw the original image, one way or the other
+        ImageView image = (ImageView) findViewById(R.id.endgame_image);
+        if(image != null)
+            image.setImageURI(selectedImage);
+        else
+            image.setImageDrawable(getResources().getDrawable(R.drawable.image));
 
-        TextView t = new TextView(this);
+        // TODO : Display game time
+
+        TextView t = (TextView) findViewById(R.id.endgame_label);
         t.setText(getString(R.string.end_infos, width, height));
-        LinearLayout layout = new LinearLayout(this);
-        layout.addView(t);
-        setContentView(layout);
     }
 }
